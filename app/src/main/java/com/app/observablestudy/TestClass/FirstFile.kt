4fb31163fd06298fe7bf0bcfc521f58577d2b389
raheez66 <raheez66@gmail.com>
2022-03-10 17:55:44 +0400

    worked on visibility modifiers for derived class

--- app/src/main/java/com/app/observablestudy/TestClass/FirstFile.kt
+++ app/src/main/java/com/app/observablestudy/TestClass/FirstFile.kt
@@ -3,13 +3,14 @@ package com.app.observablestudy.TestClass
 
 internal val a = 20
 private val b = 20
+
 //protected val c = 20 //class or member cannot be PROTECTED in top level
 public val d = 20
 
-class Accessibility{
+class Accessibility {
 
-    val  test_a = a // a is visible         // private is accessible within same file
-    val test_b = b //b is accessible        // private is accessible within same file
- // val test_c = b //d is not accessible    // class or member cannot be PROTECTED in top level
-    val test_d = d //b is accessible        // public is accessible within the project
+    val test_a = a // a is visible          // private is accessible within same module
+    val test_b = b  //b is accessible        // private is accessible within same file
+    // val test_c = c  //c is not accessible    // class or member cannot be PROTECTED in top level
+    val test_d = d  //b is accessible        // public is accessible within the project
 }
--- app/src/main/java/com/app/observablestudy/TestClass/SecondFile.kt
+++ app/src/main/java/com/app/observablestudy/TestClass/SecondFile.kt
@@ -1,12 +1,19 @@
 package com.app.observablestudy.TestClass
 
 
-public fun runProgram(){
+open class FirstClass {
 
+    internal val a = 20
+    private val b = 20
+    protected val c = 20 //class or member can be PROTECTED inside class
+    public val d = 20
 }
 
-internal val testValue =20
-class SecondFile(){
+class SecondClass : FirstClass() {
 
-    var a = testValue
+
+    val test_a = a // a is accessible          // internal is accessible within same module
+    //val test_b = b //b is not accessible    // private is not accessible in derived class
+    val test_c = c //c is accessible        // is visible in derived class
+    val test_d = d //b is accessible        // public is accessible within the project
 }
